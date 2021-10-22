dfb67d632a933191a1c9008a7758dc123075b7ed
Antoine <52282738+antwanaa@users.noreply.github.com>
2021-10-22 16:32:56 -0400

    Serial in progress

---
+++ LeftArduino.ino
@@ -0,0 +1,30 @@
+String incomingMessage = ""; // for incoming serial data
+
+void setup() {
+  Serial.begin(115200); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  // reply only when you receive data:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingMessage = Serial.readStringUntil('\n');
+    if(incomingMessage.charAt(0) == 'l'){
+      Serial.print("The LEFT Arduino is doing action: ");
+      
+      if(incomingMessage.charAt(1) == '1'){
+        Serial.println("1");
+      }
+      else if(incomingMessage.charAt(1) == '2'){
+        Serial.println("2");
+      }else if(incomingMessage.charAt(1) == '3'){
+        Serial.println("3");
+      }else{
+        Serial.println("INVALID ACTION");
+      }
+
+    }
+    delay(10);
+  }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # HCICode
 Code for our HCI Project
-hello
+
+
+Rules for messages sent through serial:
+- Message is a string
+- If desired recipient is
+    - Left wearable: start message with 'l'
+    - Right wearable: start message with 'r'
+    - computer: start message with 'c'
+    - both left and right wearable: start message with 'a'
+- finish all strings with '\n'
